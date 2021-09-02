113a5a3366764824a41f94ae15d00c32c997b129
amandadomer <amdomer@icloud.com>
2021-09-01 19:13:13 -0500

    added functions and began switch case for menu

--- atm.js
+++ atm.js
@@ -1,15 +1,37 @@
 "use strict";
 
 const bankInfo = require("./account");
+    console.log(bankInfo);
 
-console.log(bankInfo);
-
-function getBalance {
-    bankInfo.balance;
+function getBalance () {
+    console.log(bankInfo.balance);
 }
 
 function withdraw (){
+    let userEntry = pareseInt(prompt ("Enter the amount you want to withdraw"));
+    let withdrawResult = account.balance - userEntry;
+        console.log ("Your new balance is $" + withdrawResult);
 }
 
 function deposit (){
+    let userEntry = parseInt(prompt ("Enter the amount you want to deposit?"));
+    account.balance = account.balance + userEntry;
+        console.log ("Your new balance is $"+ account.balance);
 }
+
+function validatePin (){
+    let pinEntered;
+    while (pinEntered !== account.pin){
+        pinEntered = prompt ("Please enter your PIN");
+        console.log ("Incorrect PIN");
+        if (pinEntered === account.pin){
+            console.log ("Correct PIN");    
+        }
+    }
+}
+
+module.exports.balance = getBalance;
+module.exports.withdraw = withdraw;
+module.exports.deposit = deposit;
+module.exports.validate = validatePin;
+console.log(module);
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let menu = prompt ("Please choose from one of the following: 'Get Balance'\n'Withdraw'\n'Deposit'\n'Validate Pin'\n'Exit'").toLowerCase();
+switch (menu){
+case "Get Balance":
+
+    break;
+
+case "Withdraw":
+
+    break;
+
+case "Deposit":
+    
+    break;
+
+case "Validate Pin":
+    
+    break;
+    
+case "Exit":
+return menu;
+
+default:
+    prompt ("Invalid Entry");
+break;
+
+
+}
